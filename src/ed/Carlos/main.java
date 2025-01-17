e6a69922aeafcbc85db2d67c4771e76516e285e9
Carlos Martínez <carlosmg0012@gmail.com>
2025-01-17 15:40:01 +0100

    Afegit el mètode mostrarNumeros que mostra els números de l'1 al 20.

---
+++ src/ed/Carlos/main.java
@@ -0,0 +1,14 @@
+package ed.Carlos;
+
+public class main {
+    public static void main(String[] args) {
+        mostrarNumeros();
+    }
+
+    public static void mostrarNumeros() {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
